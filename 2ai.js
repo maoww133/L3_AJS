b1790d4a15f50dab132f4a5cc694ff9036202203
maoww133 <cwer133@gmail.com>
2026-01-06 08:14:19 +0300

    Update 2ai.js

--- 2ai.js
+++ 2ai.js
@@ -14,14 +14,12 @@ function getDataFromAPI(url, callback) {
     });
 }
 
-// Использование
 getDataFromAPI('https://jsonplaceholder.typicode.com/posts', (error, postsData) => {
   if (error) {
     console.error('Произошла ошибка при загрузке posts:', error.message);
     return;
   }
 
-  // Сортировка по убыванию длины заголовка
   const postsSortedByTitleLength = postsData.sort((a, b) => 
     b.title.length - a.title.length
   );
@@ -29,3 +27,4 @@ getDataFromAPI('https://jsonplaceholder.typicode.com/posts', (error, postsData) 
   console.log('Посты, отсортированные по длине заголовка:');
   console.log(postsSortedByTitleLength);
 });
+
