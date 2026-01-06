283827a83d196f7fd920f038dc245ba872326529
maoww133 <cwer133@gmail.com>
2026-01-06 08:12:33 +0300

    Update 2ai.js

--- 2ai.js
+++ 2ai.js
@@ -1,36 +1,31 @@
-const https = require('https');
-
 function getDataFromAPI(url, callback) {
-  https.get(url, (response) => {
-    let rawData = '';
-    
-    response.on('data', (chunk) => {
-      rawData += chunk;
-    });
-    
-    response.on('end', () => {
-      try {
-        const parsedData = JSON.parse(rawData);
-        callback(null, parsedData);
-      } catch (parseError) {
-        callback(parseError);
+  fetch(url)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`Ошибка HTTP: ${response.status}`);
       }
+      return response.json();
+    })
+    .then(data => {
+      callback(null, data);
+    })
+    .catch(error => {
+      callback(error);
     });
-    
-  }).on('error', (error) => {
-    callback(error);
-  });
 }
 
+// Использование
 getDataFromAPI('https://jsonplaceholder.typicode.com/posts', (error, postsData) => {
   if (error) {
-    console.error('Произошла ошибка при загрузке posts:', error);
+    console.error('Произошла ошибка при загрузке posts:', error.message);
     return;
   }
-  
-  const postsSortedByTitleLength = postsData.sort((postA, postB) => 
-    postB.title.length - postA.title.length
+
+  // Сортировка по убыванию длины заголовка
+  const postsSortedByTitleLength = postsData.sort((a, b) => 
+    b.title.length - a.title.length
   );
-  
-  console.log('Posts отсортированные по длине title:', postsSortedByTitleLength);
+
+  console.log('Посты, отсортированные по длине заголовка:');
+  console.log(postsSortedByTitleLength);
 });
