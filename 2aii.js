7a99b5d979f98ad46f57b8989757d1f5812a05f1
maoww133 <cwer133@gmail.com>
2026-01-06 08:13:58 +0300

    Update 2aii.js

--- 2aii.js
+++ 2aii.js
@@ -1,43 +1,36 @@
-const https = require('https');
-
 function retrieveAndSortComments(callback) {
   const apiUrl = 'https://jsonplaceholder.typicode.com/comments';
-  
-  https.get(apiUrl, (response) => {
-    let collectedData = '';
-    
-    response.on('data', (chunk) => {
-      collectedData += chunk;
-    });
-    
-    response.on('end', () => {
-      try {
-        const commentsArray = JSON.parse(collectedData);
-        const alphabeticallySortedComments = commentsArray.sort((first, second) => 
-          first.name.localeCompare(second.name)
-        );
-        callback(null, alphabeticallySortedComments);
-      } catch (parseError) {
-        callback(parseError, null);
+
+  fetch(apiUrl)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`Ошибка HTTP: ${response.status}`);
       }
+      return response.json();
+    })
+    .then(comments => {
+      const alphabeticallySortedComments = comments.sort((a, b) => 
+        a.name.localeCompare(b.name)
+      );
+      callback(null, alphabeticallySortedComments);
+    })
+    .catch(error => {
+      callback(error, null);
     });
-    
-  }).on('error', (networkError) => {
-    callback(networkError, null);
-  });
 }
 
 retrieveAndSortComments((error, commentsSortedByName) => {
   if (error) {
-    console.error('Не удалось загрузить комментарии:', error);
+    console.error('Не удалось загрузить комментарии:', error.message);
     return;
   }
-  
-  console.log('Список комментариев, упорядоченный по имени автора:');
-  
+
+  console.log('Список комментариев, упорядоченный по имени автора:\n');
+
   commentsSortedByName.forEach(comment => {
-    console.log(`Имя: ${comment.name}`);
-    console.log(`Адрес: ${comment.email}`);
+    console.log(`Имя:       ${comment.name}`);
+    console.log(`Адрес:     ${comment.email}`);
     console.log(`Содержание: ${comment.body}`);
+    console.log('─'.repeat(60));
   });
 });
